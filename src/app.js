638c24ce04f2afa00ae36b4a5a9af6e9958a9426
Tony Mao <tianguohut@163.com>
2016-05-24 16:24:06 +0800

    Adding 3 pages

--- src/app.js
+++ src/app.js
@@ -5,15 +5,20 @@ var myApp = angular.module('myApp', ['ui.router', 'ngAnimate']);
 myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
   $stateProvider
-    .state(
-      'home', {
-        url: '/home',
-        template: '<h2>Hello World!!!!</h2>'
-      })
-    .state(
-      'event', {
-        url: '/event',
-        template: '<h3>Event</h3>'
-      }
-    );
+    .state('home', {
+      url: '/home',
+      templateUrl: 'src/pages/home.html'
+    })
+    .state('transportationAndHotel', {
+      url: '/transportation-and-hotel',
+      templateUrl: 'src/pages/transportation-and-hotel.html'
+    })
+    .state('spectators', {
+      url: '/spectators',
+      templateUrl: 'src/pages/spectators.html'
+    })
+    .state('contactUs', {
+      url: '/contact-us',
+      templateUrl: 'src/pages/contact-us.html'
+    });
 }]);
