132baa7966e1825488b2e725fbc4c2f32df947a3
Tony Mao <tianguohut@163.com>
2016-05-27 16:51:49 +0800

    Adding download feature

---
+++ download.php
@@ -0,0 +1,24 @@
+<?php
+if (isset($_GET['f'])) {
+  switch ($_GET['f']) {
+    case 'ULTRA-TRAIL_NINGHAI_map_42k':
+      $file = './assets/ULTRA-TRAIL_NINGHAI_map_42k.jpg';
+      break;
+    case 'ULTRA-TRAIL_NINGHAI_map_100k':
+      $file = './assets/ULTRA-TRAIL_NINGHAI_map_100k.jpg';
+      break;
+    default:
+      exit;
+  }
+  if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+  }
+}
+exit;
