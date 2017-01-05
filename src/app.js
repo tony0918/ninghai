a50a9efd0cef656ff692b6a1e0571a15af5a2559
Tony Mao <huiliumao@gmail.com>
2017-01-05 23:18:35 +0800

    Adding Lijiang race

--- src/app.js
+++ src/app.js
@@ -7,13 +7,13 @@ function trackPageView(pageURL) {
 
 var myApp = angular.module('myApp', ['ui.router', 'ngAnimate']);
 
-myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
     $stateProvider
         .state('home', {
             url: '/home',
             templateUrl: 'src/pages/home.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/home');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -21,7 +21,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('transportationAndHotel', {
             url: '/transportation-and-hotel',
             templateUrl: 'src/pages/transportation-and-hotel.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/transportation-and-hotel');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -29,7 +29,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('spectators', {
             url: '/spectators',
             templateUrl: 'src/pages/spectators.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/spectators');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -45,7 +45,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('aboutUs', {
             url: '/about-us',
             templateUrl: 'src/pages/about-us.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/about-us');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -53,7 +53,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ultraTrailCourse', {
             url: '/ultra-trail/course',
             templateUrl: 'src/pages/ultra-trail-course.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ultra-trail/course');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -61,7 +61,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ultraTrailRegulation', {
             url: '/ultra-trail/regulation',
             templateUrl: 'src/pages/ultra-trail-regulation.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ultra-trail/regulation');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -69,7 +69,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ultraTrailRegistration', {
             url: '/ultra-trail/registration',
             templateUrl: 'src/pages/ultra-trail-registration.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ultra-trail/registration');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -77,7 +77,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ultraTrailSchedule', {
             url: '/ultra-trail/schedule',
             templateUrl: 'src/pages/ultra-trail-schedule.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ultra-trail/schedule');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -85,7 +85,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ultraTrailResult', {
             url: '/ultra-trail/result',
             templateUrl: 'src/pages/ultra-trail-result.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ultra-trail/result');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -93,7 +93,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('news', {
             url: '/news',
             templateUrl: 'src/pages/news.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/news');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -101,7 +101,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('newsItem1', {
             url: '/news/1',
             templateUrl: 'src/pages/news/news1.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/news/1');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -109,7 +109,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('newsItem2', {
             url: '/news/2',
             templateUrl: 'src/pages/news/news2.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/news/2');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -117,7 +117,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('newsItem3', {
             url: '/news/3',
             templateUrl: 'src/pages/news/news3.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/news/3');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -125,7 +125,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('newsItem4', {
             url: '/news/4',
             templateUrl: 'src/pages/news/news4.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/news/4');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -133,7 +133,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('newsItem5', {
             url: '/news/5',
             templateUrl: 'src/pages/news/news5.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/news/5');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -141,7 +141,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('gallery', {
             url: '/gallery',
             templateUrl: 'src/pages/gallery.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/gallery');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -149,7 +149,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('galleryNinghaiTrail', {
             url: '/gallery-ninghai-trail',
             templateUrl: 'src/pages/gallery-ninghai-trail.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/gallery-ninghai-trail');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -157,7 +157,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('voluntary', {
             url: '/voluntary',
             templateUrl: 'src/pages/voluntary.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/voluntary');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -165,7 +165,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ultraTrailFaq', {
             url: '/ultra-trail/faq',
             templateUrl: 'src/pages/ultra-trail-faq.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ultra-trail/faq');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -173,7 +173,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ninghaiTrail', {
             url: '/ninghai-trail',
             templateUrl: 'src/pages/ninghaitrail/ninghai-trail-course.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ninghai-trail');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -181,7 +181,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ninghaiTrailResult', {
             url: '/ninghai-trail/result',
             templateUrl: 'src/pages/ninghaitrail/ninghai-trail-result.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ninghai-trail/result');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -197,7 +197,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ninghaiTrailRegistration', {
             url: '/ninghai-trail/registration',
             templateUrl: 'src/pages/ninghaitrail/ninghai-trail-registration.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ninghai-trail/registration');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -205,7 +205,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ninghaiTrailSchedule', {
             url: '/ninghai-trail/schedule',
             templateUrl: 'src/pages/ninghaitrail/ninghai-trail-schedule.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ninghai-trail/schedule');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -213,7 +213,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ninghaiTrailRegulation', {
             url: '/ninghai-trail/regulation',
             templateUrl: 'src/pages/ninghaitrail/ninghai-trail-regulation.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ninghai-trail/regulation');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -221,7 +221,7 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('ninghaiTrailTransportationAndHotel', {
             url: '/ninghai-trail/transportation-and-hotel',
             templateUrl: 'src/pages/ninghaitrail/ninghai-trail-transportation-and-hotel.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/ninghai-trail/transportation-and-hotel');
                 $('.page-content')[0].scrollTop = 0;
             }
@@ -229,8 +229,48 @@ myApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $
         .state('lijiangSkyviewCourse', {
             url: '/lijiang/course',
             templateUrl: 'src/pages/lijiang/lijiang-skyview-course.html',
-            onEnter: function() {
+            onEnter: function () {
                 trackPageView('/lijiang/course');
+                $('.page-content')[0].scrollTop = 0;
+            }
+        })
+        .state('lijiangSkyviewUltra50k', {
+            url: '/lijiang/ultra50k',
+            templateUrl: 'src/pages/lijiang/lijiang-skyview-ultra50k.html',
+            onEnter: function () {
+                trackPageView('/lijiang/ultra50k');
+                $('.page-content')[0].scrollTop = 0;
+            }
+        })
+        .state('lijiangSkyviewSkyrace21k', {
+            url: '/lijiang/skyrace21k',
+            templateUrl: 'src/pages/lijiang/lijiang-skyview-skyrace21k.html',
+            onEnter: function () {
+                trackPageView('/lijiang/skyrace21k');
+                $('.page-content')[0].scrollTop = 0;
+            }
+        })
+        .state('lijiangSkyviewAccomodation', {
+            url: '/lijiang/accomodation',
+            templateUrl: 'src/pages/lijiang/lijiang-skyview-accomodation.html',
+            onEnter: function () {
+                trackPageView('/lijiang/accomodation');
+                $('.page-content')[0].scrollTop = 0;
+            }
+        })
+        .state('lijiangSkyviewSponsor', {
+            url: '/lijiang/sponsor',
+            templateUrl: 'src/pages/lijiang/lijiang-skyview-sponsor.html',
+            onEnter: function () {
+                trackPageView('/lijiang/sponsor');
+                $('.page-content')[0].scrollTop = 0;
+            }
+        })
+        .state('galleryLijiang', {
+            url: '/lijiang/gallery',
+            templateUrl: 'src/pages/lijiang/lijiang-skyview-gallery.html',
+            onEnter: function () {
+                trackPageView('/lijiang/gallery');
                 $('.page-content')[0].scrollTop = 0;
             }
         });
